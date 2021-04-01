f75762b58b4c74b448a75125dabf5f8d12387126
virginsky <77185864+virginsky@users.noreply.github.com>
2021-04-01 18:03:05 +0900

    error
    fail

---
+++ Basic Mathematics for Artificial Intelligence.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+## p28 (Code) 수학 코딩해보기
+x = [a for a in range(6)]
+f = [-x*x + 4*x + 1 for b in range(6)]
+plt.plot(x, f)      #구간 : x = 0~5까지
+plt.show()
